059a7aa62065be8151a63a5fdce1dbe04867aa14
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-07-01 19:14:16 +0300

    Add files via upload

---
+++ kek.py
@@ -0,0 +1,5 @@
+print('Цена за одну пиццу : 500')
+user_input = int(input('Укажите количество человек: '))
+user_input2 = int(input(' Укажите количество имеющихся денег: '))
+result = user_input2 / 500
+print(f'Для {user_input} человек, как раз {result} пиццы ')
