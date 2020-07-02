33b0e22f427ccca49a18386b05ad982d99387b03
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-07-02 17:09:48 +0300

    Add files via upload

---
+++ lesson4_2.py
@@ -0,0 +1,19 @@
+first_list = [1,2,5,7]
+second_list = [0]
+third_list = [3,2]
+first_list[3]
+second_list[0]
+third_list[1]
+
+if first_list[3] % 2 == 0:
+    print(f'Последнее число {first_list[3]} - чётное ')
+else:
+     print(f'Последнее число {first_list[3]} - Нечётное ')
+if second_list[0] % 2 == 0:
+    print(f'Последнее число {second_list[0]} - чётное ')
+else:
+     print(f'Последнее число {second_list[0]} - Нечётное ')    
+if third_list[1] % 2 == 0:
+    print(f'Последнее число {third_list[1]} - чётное ')
+else:
+     print(f'Последнее число {third_list[1]} - Нечётное ')
