bf8eb1a6a8d9974a645c3d4edc45bae64e080468
raferalston <like2pwnyou@yandex.ru>
2020-07-02 18:19:55 +0300

    Update lesson4_3.py

--- lesson4_3.py
+++ lesson4_3.py
@@ -1,3 +1,4 @@
+# Используй ветвления для разных ответов - подсмотри решение https://kidkodschool.github.io/lesson_4_ans_3.html
 print('Цена за одну пиццу : 500')
 user_input = int(input('Укажите количество человек: '))
 user_input2 = int(input(' Укажите количество имеющихся денег: '))
