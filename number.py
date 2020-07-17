4b297a53d4ba8b01fe7fed2ad6590df89a17f7f3
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-07-17 11:39:11 +0300

    Delete number.py

--- number.py
+++
@@ -1,9 +0,0 @@
-user_input = int(input('Введите первое число: '))
-user_input2 = int(input('Введите второе число: '))
-result = user_input + user_input2
-result2 = user_input * user_input2
-if result < 1000:
-    print(f'Сумма {user_input} и {user_input2} = {result}')
-else:
-    print (f'Произведение {user_input} и {user_input2} = {result2}')
-    
