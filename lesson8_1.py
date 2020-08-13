8bd4f23ffe82cb2b5bcd8d4474403ffb7f876592
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-08-13 20:21:38 +0300

    Add files via upload

--- lesson8_1.py
+++ lesson8_1.py
@@ -1,23 +1,22 @@
-#cписал
+sentence ='Fair is foul, and foul is fair: Hover through the fog and filthy air.'
 alfabet = 'qwertyuiopasdfghjklzxcvbnm'
 
-def count_upper(w):
+def lower(w):
     count = 0
-    for l in w:
-        for a in alfabet.upper():
-            if l == a:
-             count += 1
-    return count
-
-def count_lower(w):
-    count = 0
-    for l in w:
+    for i in w:
         for a in alfabet.lower():
-            if l == a:
-             count += 1
+            if i == a:
+                count += 1
     return count
 
-sentence = 'Fair is foul, and foul is fair: Hover through the fog and filthy air.'
+def upper(w):
+    count = 0
+    for i in w:
+        for a in alfabet.upper():
+            if i == a:
+                count += 1
+    return count
 
-print(f'Total uppercase letters in sentence is: {count_upper(sentence)}')
-print(f'Total lower letters in sentence is: {count_lower(sentence)}')
+
+print(f'Total uppercase letters in sentence is: {upper(sentence)}')
+print(f'Total lowercase letters in sentence is: {lower(sentence)}')
--- lesson8_2.py
+++ lesson8_2.py
@@ -1,8 +1,14 @@
-def build_pyramid(w):
-    brick 
+def build_pyramid(r):
+    for i in range(r // 2):
+        print('*' * (i + 1))
 
-input_of_number = int(input('Введите число этажей у пирамиды: '))
-answer = ' '
+    for i in range((r // 2) + 1,0,-1):
+        print('*' * (i))
 
-if answer % 2 != 0:
+user_input = int(input('Введите число: '))
+
+if user_input % 2 == 0:
     print('Введите нечётное число')
+else:
+    build_pyramid(user_input)
+
