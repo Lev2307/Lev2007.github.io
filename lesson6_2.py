7644fd7f24b1ce25cf943d840e759e3a2d9a114c
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-08-13 20:17:18 +0300

    Add files via upload

--- lesson6_2.py
+++ lesson6_2.py
@@ -1,4 +1,6 @@
-#не знаю как сделать =/
-def reverse_string(string):
-    tnp = ''
-    
+def reverse_string(s):
+    return s[::-1]
+
+user_input = str(input('Введите строку и я переверну её: '))
+print(reverse_string(user_input))
+
