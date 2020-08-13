5260733953768a8ad99e3782a72aba1b2d964161
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-08-13 20:19:24 +0300

    Add files via upload

--- lesson6_3.py
+++ lesson6_3.py
@@ -1,10 +1,9 @@
-#я не знаю как эту ошибку исправить =/
 def palindrome(string):
     if string == string[::-1]:
         return True
     return False
 
-user_input - input('Введи слово мб это палиндром')
+user_input = input('Введи слово мб это палиндром')
 
 if palindrome(user_input):
     print(f'Это {user_input} - палиндром')
