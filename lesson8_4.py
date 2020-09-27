5e6627cf3ced16861cb99823470e9512a99f75b8
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-09-27 15:51:17 +0300

    Add files via upload

--- lesson8_4.py
+++ lesson8_4.py
@@ -1,4 +1,6 @@
+from random import randint
 
+obs_percent = 50
 def build_template_input():
         """
         output: return str, built-in input() function
@@ -9,15 +11,13 @@ def build_field(size):
         field_size = ['_'] * size # Создаем ряд на поле
         field = [] # Создаем пустое поле
 
-        print(f'Пустое поле до добавления в него рядов {field}\n')
         for _ in range(size):
             field.append(field_size[:]) # Добавляем нужное количество рядов в нашем поле
-        print(f'Поле после добавления в него рядов')
-        [print(row) for row in field] # Эту часть можешь удалить как поймешь процесс отрисовки поля
+        # print(f'Поле после добавления в него рядов')
+        # [print(row) for row in field] # Эту часть можешь удалить как поймешь процесс отрисовки поля
         return field # Возвращаем сгенерированное поле
 
 def move_player(direction, field):
-    
     if direction == 'right':
         for row in range(len(field)):
             for col in range(len(field)):
@@ -33,30 +33,47 @@ def move_player(direction, field):
                     field[row][col-1] = 'x' 
                     break
 
+    if direction == 'up':
+        for row in range(len(field)):
+            for col in range(len(field)):
+                if field[row][col] == 'x':
+                    field[row][col] = '_'
+                    field[row-1][col] = 'x' 
+                    break
     if direction == 'down':
         for row in range(len(field)):
             for col in range(len(field)):
                 if field[row][col] == 'x':
                     field[row][col] = '_'
                     field[row+1][col] = 'x' 
-                    break
+                    return field
     return field
 
+
+def build_obstacles(field1, field):
+    for row in range(len(field)):
+        for col in range(len(field)):
+            if field1 < 5:
+                field[row][col] = 'o'
+                
+    return field
 def game():
    
     size_of_template = build_template_input()
     field = build_field(size_of_template)
 
-    print(f'\nДобавляем стартовую локацию')
+    field1 = randint(0, 100)
     field[0][0] = 'x'
     [print(row) for row in field] # Эту часть можешь удалить как поймешь процесс отрисовки поля
     
     gameStatus = True
     while gameStatus:
-        user_direction = input('\nВведите направление: ')
+        user_direction = input('\nВыберите одно из направлений: up right down left или exit для выхода ')
 
+        field = obstacles(field1, field)
         field = move_player(user_direction, field)
+            
+        if user_direction == 'exit':
+            gameStatus = False
         [print(row) for row in field] # Эту часть можешь удалить как поймешь процесс отрисовки поля
-        
-
 game()
