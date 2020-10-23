8f2d6a7cfc95367a0f706ad0ebb919ba6157bb4c
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-10-23 18:07:16 +0300

    Add files via upload

---
+++ krestiki-noliki.py
@@ -0,0 +1,50 @@
+from tkinter import *
+#TODO: change import method, to fropm tkinter import module
+
+# Constants
+CANVAS_SIZE = 600
+FIGURE_SIZE = CANVAS_SIZE / 3
+RATIO = CANVAS_SIZE // FIGURE_SIZE
+BG_COLOR = 'white'
+EMPTY = NONE
+
+# players setup
+x = 'player 1'
+o = 'player 2'
+FIRST_PLAYER = X
+
+class Board(Tk):
+    def __init__(self, start_player):
+        super().__init__()
+        self.canvas = Canvas(height=CANVAS_SIZE, width=CANVAS_SIZE, bg=BG_COLOR)
+        self.canvas.pack()
+        self.figure_size = FIGURE_SIZE
+        self.current_player = start_player
+        # self.canvas.bind('<Button-1>', self.click_event)
+        self.board = [
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+    def build_grid(self):
+        x = CANVAS_SIZE // RATIO
+        y1 = 0
+        y2 = CANVAS_SIZE
+        for _ in range(2):
+            self.canvas.create_line(x, y1, x, y2)
+            self.canvas.create_line(y1, x, y2, x)
+            x += CANVAS_SIZE // RATIO
+
+    def render_cross(self, posX, posY):
+        pass
+
+    def render_circle(self, posX, posY):
+        pass
+
+    def winner(self, player=None):
+        pass
+
+
+game_v1 = Board(start_player=FIRST_PLAYER)
+game_v1.build_grid()
+game_v1.mainloop()                               
