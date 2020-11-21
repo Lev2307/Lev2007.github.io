d7ad180117340dcedba2fdaa79cd10dcd5237d8c
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2020-11-21 17:14:26 +0300

    Add files via upload

--- programm_for_olimpiada.py
+++ programm_for_olimpiada.py
@@ -1,49 +1,56 @@
 from tkinter import *
 
 master = Tk()
-master.resizable(0,0)
+master.resizable(0, 0)
+master.geometry("400x400")
 master.title('OLIMPICS OF PYTHON')
 
-first_answer = {'-3 + 3': '0'}
-second_answer = {'7 * 2': '14'}
-third_answer = {'0 + 1' : '1'}
-answers = []
-first_answer_k = first_answer.values()
-second_answer_k = second_answer.values()
-third_answer_k = third_answer.values()
+# constants
+CANVAS_SIZE = 400
 
-def first_text():
-    text = Label(master, text="Вам осталось ответить на 3 вопроса", font='Arial 20')
-    text.pack()
-
-def first_text_of_question():
-    question = Label(master, text="-3 + 3", font='Arial 15', bg="red")
-    question.pack()
-
-def add_element_event(event=None):
-    current = entry_field.get()
-    entry_field.delete(0, END)
-    entry_field.insert(0, str(current) + str())
-    answers.append(current)
-    print(answers)
+q_and_a = {
+    '8+8': '16',
+    '2-2': '0',
+    '3+3': '6'
+}
+canvas = Canvas(width=CANVAS_SIZE, height=CANVAS_SIZE, bg='#a3907e')
+canvas.pack()
 
 
-def second_text():
-    text = Label(master, text="Вам осталось ответить на 2 вопроса", font='Arial 20')
-    text.pack()
+class Board():
+    def __init__(self):
+        self.canvas = canvas
+        self.btn_teacher = Button(self.canvas, padx=13.5, pady=20, width=15, height=5, text='Учитель', bg='#713045', fg='white', command=self.delete_all_teacher)
+        self.btn_student = Button(self.canvas , padx=13.5, pady=20, width=15, height=5, text='Ученик', bg='#1e6262', fg='white', command=self.delete_all_student)
+        self.btn_teacher.place(x=50, y=160)
+        self.btn_student.place(x=220, y=160)
 
-def second_text_of_question():
-    question = Label(master, text="7 * 2", font='Arial 15', bg="red")
-    question.pack()
+    def delete_all_student(self):
+        self.btn_teacher.destroy()
+        self.btn_student.destroy()
+        Student()
 
-def btn():
-    btn = Button(master, text='Ответ', padx=15, pady=7, command=lambda: add_element_event())
-    btn.pack()
+    def delete_all_teacher(self):
+        self.btn_teacher.destroy()
+        self.btn_student.destroy()
+        self.teachers_page()
 
-first_text()
-first_text_of_question()
-entry_field = Entry(master,width=40,borderwidth=5)
-entry_field.pack()
-btn()    
+    # def students_page(self):
+    #     self.first_question()
 
+
+class Student(Board):
+    def __init__(self):
+        self.canvas = canvas
+        self.entry = Entry(self.canvas, width=30, borderwidth=3)
+        self.entry.place(x=100, y=200)
+        self.btn_answer = Button(self.canvas, width=10, height=2, text='Ответ: ')
+        self.btn_answer.place(x=160, y=250)
+        self.questions = q_and_a.keys()
+        self.answers = q_and_a.values()
+    
+    def printed_form():
+        pass
+
+b = Board()
 master.mainloop()
