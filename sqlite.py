bbf04003995a9b05f599da949db916d784f214f1
Lev2307 <57072195+Lev2307@users.noreply.github.com>
2021-01-29 18:26:13 +0300

    Add files via upload

---
+++ sqlite.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+c.execute('CREATE TABLE user (name text, email text)')
+
+c.execute("INSERT INTO user VALUES ('Mike', 'mike@mike.com')")
+
+conn.commit()
+conn.close()
